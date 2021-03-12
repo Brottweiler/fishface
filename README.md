519cfc7918f4525737a70d786b85d1f8268ba5f1
Brottweiler <brottsie@gmail.com>
2021-03-12 22:59:43 +0100

    Add folder and git info to right prompt

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Shows an ASCII fish which is blue or green when in a git repo.
 
 ![fishface theme](https://f.cloud.github.com/assets/66143/1224622/ec9660d8-2750-11e3-9c96-cb7a5a69eada.png)
+
+## Git branch info
+
+# https://gist.github.com/721241c7c34f841eed07
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,20 +1,54 @@
 # name: FishFace
+# https://github.com/Brottweiler/fishface
+
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+	echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+	echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function parse_git_branch
+	set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+	set -l git_status (git status -s)
+
+	if test -n "$git_status"
+		echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+	else
+		echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+	end
 end
 
 function fish_prompt
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
+	set -l blue (set_color -o blue)
+	set -l green (set_color -o green)
 
-  if [ (_git_branch_name) ]
-    echo -n -s "$green><(((\"> "
-  else
-    echo -n -s "$blue><(((\"> "
-  end
+	if [ (_git_branch_name) ]
+		echo -n -s "$green><(((\"> "
+	else
+		echo -n -s "$blue><(((\"> "
+	end
+end
+
+function fish_right_prompt
+	set -l blue (set_color -o blue)
+	set -l green (set_color -o green)
+
+	set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+
+	if test -n "$git_dir"
+		set_color normal
+		set_color blue
+		echo -n -s (prompt_pwd) " " (parse_git_branch) " "
+		set_color normal
+	else
+		set_color normal
+		set_color blue
+		echo -n -s (prompt_pwd) " "
+		set_color normal
+	end
 end
